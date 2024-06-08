24475cc707439201a7cabb79fdd62123e6fcc4bf
shubham <maskeshubham555@gmail.com>
2024-06-08 17:59:57 +0530

    Added my booking page

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function formOpen() {
     var test = document.getElementById("test")
     test.style.display = "block";
-
 }
 
 function closeFrom(){
@@ -17,15 +16,28 @@ function donationFromClose(){
     var donationform1 = document.getElementById("donation-form")
     donationform1.style.display = "none";
 }
+function myBookingOpen() {
+    var bookingScreen = document.getElementById("booking-list")
+    bookingScreen.style.display = "block";
+}
+
+function myBookingClose(){
+    var bookingScreen1 = document.getElementById("booking-list")
+    bookingScreen1.style.display = "none";
+}
+
+
 
 var userDeatilsArray = [];
 
 function getData () {
-    const Date = document.getElementById("date").value
-    const timeSlot = document.getElementById("time").value
-    const darshanType = document.getElementById("type").value
-    const passType = document.getElementById("passtype").value
-    const noOfPerson = document.getElementById("noOfPerson").value
+    console.log("called")
+    var Date = document.getElementById("date").value
+    var timeSlot = document.getElementById("time").value
+    var darshanType = document.getElementById("type").value
+    var passType = document.getElementById("passType").value
+    var noOfPerson = document.getElementById("noOfPerson").value
+    var bookingCard = document.getElementById("booking-card")
 
     const Bookingdetails = {
         date : Date,
@@ -35,9 +47,24 @@ function getData () {
         noofperson : noOfPerson
     } 
 
-    // localStorage.setItem(Bookingdetails)
-
     userDeatilsArray.push(Bookingdetails);
-    console.log(Bookingdetails)
-    alert("booked")
+    
+    localStorage.setItem("userBookingDetail", JSON.stringify(userDeatilsArray));
+    // console.log(JSON.parse(localStorage.getItem("userBookingDetail")));
+ var data = localStorage.getItem("userBookingDetail")
+ var data2 =JSON.parse(data)
+ console.log(data2.date)
+
+    alert("Pass Booked Successfully!!")
+
+    
+    bookingCard.innerHTML += ` <div class="booking-details">
+    <li>Date : ${data2.date}</li>
+    <li>Slot Time : ${data2.timeslot}</li>
+    <li>Type : ${data2.darshantype}</li>
+    <li>Pass Type : ${data2.passtype}</li>
+    <li>No. Of Person : ${data2.noofperson}</li>
+    </div>`
+    
+
 }
